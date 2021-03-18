3e9822ad776e6f79bfcbdda25aeb45c7c91667a0
konglongpai <506398310@qq.com>
2021-03-18 09:47:40 +0000

    添加crf验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,7 @@
 
         <div class="card-body">
             <form action="">
+            {{ csrf_field() }}
                 <div class="form-group">
                     <label for="name">姓名:</label>
                     <input type="text" name="name" class="form-control" value="{{ old('name')}}">
